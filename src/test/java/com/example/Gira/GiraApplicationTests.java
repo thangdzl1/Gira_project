6c17ef9cbde384861e9a920b877f445515b14597
BQcThang <quocthang6a1@gmail.com>
2023-07-29 13:29:57 +0700

    initial commit

---
+++ src/test/java/com/example/Gira/GiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
